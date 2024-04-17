d76364a48c6d0a72b6d5ab1f8774060da6add2ee
arhab213 <medriadarhab@gmail.com>
2024-04-16 16:53:40 -1100

    Dependencies initialization only Nothing else

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Component/SIdeBar/SIdebar";
+import React from 'react'
+import SIdebar from "./Component/SIdeBar/SIdebar";
+function App() {
+  return <>
+<SIdebar/>
+  </>
+}
+
+export default App
