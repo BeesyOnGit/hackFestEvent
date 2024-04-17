a44dd9a33e8b3df40a8103f63381e25a79955841
BeesyOnGit <samysahraoui697@gmail.com>
2024-04-17 02:33:16 +0100

    final side nav

--- Frontend/src/Pages/Profs/Prof.jsx
+++ Frontend/src/Pages/Profs/Prof.jsx
@@ -4,9 +4,9 @@ import SideNav from "../../Component/SideNav/SideNav";
 
 function Prof(props) {
     const navElems = [
-        { icon: "ii", name: "notes", link: "/prof/notes" },
-        { icon: "ii", name: "nouvelles", link: "/prof/feed" },
-        { icon: "ii", name: "reservation", link: "/prof/reservation-lab" },
+        { icon: "fi fi-sr-document", name: "notes", link: "/prof/notes" },
+        { icon: "fi fi-sr-newspaper", name: "nouvelles", link: "/prof/feed" },
+        { icon: "fi fi-br-calendar-day", name: "reservation", link: "/prof/reservation-lab" },
     ];
     return (
         <div style={{ width: "100%", height: "100vh", display: "flex" }}>
